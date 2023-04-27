3a9d3e19127a1ee7c2b055bc8690ae15cfdc41c4
YacineTrousselle <yacinetrousselle@gmail.com>
2023-04-27 10:56:14 +0200

    LA DB PTN

--- app/src/main/java/fr/ceri/chomageen2mots/webservice/SearchResult.java
+++ app/src/main/java/fr/ceri/chomageen2mots/webservice/SearchResult.java
@@ -5,6 +5,8 @@ import com.squareup.moshi.Json;
 import java.util.ArrayList;
 import java.util.List;
 
+import fr.ceri.chomageen2mots.database.Offre;
+
 public class SearchResult {
     @Json(name = "resultats")
     public List<Offre> resultats = new ArrayList<>();
