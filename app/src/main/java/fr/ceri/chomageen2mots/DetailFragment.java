d923bd35fad97e5747c8794a8928faed1f0354e8
YacineTrousselle <yacinetrousselle@gmail.com>
2023-05-11 22:30:06 +0200

    fix de ses morts - jsp pk cpt

--- app/src/main/java/fr/ceri/chomageen2mots/DetailFragment.java
+++ app/src/main/java/fr/ceri/chomageen2mots/DetailFragment.java
@@ -1,6 +1,7 @@
 package fr.ceri.chomageen2mots;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -13,6 +14,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.squareup.picasso.Picasso;
@@ -23,28 +25,29 @@ public class DetailFragment extends Fragment {
     String id;
     private TextView jobInfo, jobDescription, jobTitle;
     private ImageView imageView;
-    private MenuItem favToogleMenuItem;
+    private MutableLiveData<MenuItem> favToogleMenuItem = new MutableLiveData<>();
     private DetailViewModel detailViewModel;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
         if (getArguments() != null) {
             id = getArguments().getString("id");
         }
-        setHasOptionsMenu(true);
     }
 
     @Override
     public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
-        favToogleMenuItem = menu.findItem(R.id.fav_toogle);
-        favToogleMenuItem.setVisible(true);
+        favToogleMenuItem.setValue(menu.findItem(R.id.fav_toogle));
+        favToogleMenuItem.getValue().setVisible(true);
         super.onCreateOptionsMenu(menu, inflater);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.details_fragment, container, false);
+
         detailViewModel = new ViewModelProvider(this,
                 new DetailViewModelFactory(requireActivity().getApplication(), id)).get(DetailViewModel.class);
 
@@ -69,22 +72,28 @@ public class DetailFragment extends Fragment {
                         } else {
                             Picasso.get().load(R.drawable.c).into(imageView);
                         }
-                        detailViewModel.getIsFavorite().observe(getViewLifecycleOwner(),
-                                isFav -> {
-                                    if (isFav) {
-                                        favToogleMenuItem.setTitle("Supprimer");
-                                        favToogleMenuItem.setOnMenuItemClickListener(item -> {
-                                            detailViewModel.deleteFavorite();
-                                            return true;
-                                        });
-                                    } else {
-                                        favToogleMenuItem.setTitle("Ajouter");
-                                        favToogleMenuItem.setOnMenuItemClickListener(item -> {
-                                            detailViewModel.addFavorite(offre);
-                                            return true;
-                                        });
-                                    }
+                        favToogleMenuItem.observe(getViewLifecycleOwner(),
+                            menuItem -> {
+                                if (menuItem != null) {
+                                    detailViewModel.getIsFavorite().observe(getViewLifecycleOwner(),
+                                            isFav -> {
+                                                if (isFav) {
+                                                    menuItem.setTitle("Supprimer");
+                                                    menuItem.setOnMenuItemClickListener(item -> {
+                                                        detailViewModel.deleteFavorite();
+                                                        return true;
+                                                    });
+                                                } else {
+                                                    menuItem.setTitle("Ajouter");
+                                                    menuItem.setOnMenuItemClickListener(item -> {
+                                                        detailViewModel.addFavorite(offre);
+                                                        return true;
+                                                    });
+                                                }
+                                            }
+                                    );
                                 }
+                            }
                         );
                     }
                 }
